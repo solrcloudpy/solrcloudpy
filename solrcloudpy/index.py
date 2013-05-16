dea598e166bc1ec2b8f060dc6cb917d93e4b5d88
dfdeshom <didier@cogtree.com>
2013-05-16 16:47:00 -0500

    add batchadder, rase exception when solr returns one

--- solrcloudpy/index.py
+++ solrcloudpy/index.py
@@ -1,12 +1,16 @@
 from connection import ZConnection, HTTPConnection
 from requests.exceptions import *
 from requests.models import Response
+from contextlib import contextmanager
 
 import datetime as dt
 import requests
 import urlparse
 import json
 
+import logging
+
+log = logging.getLogger('solr')
 dthandler = lambda obj: obj.isoformat() if isinstance(obj, dt.datetime) else None
 
 class SolrResponse(object):
@@ -48,7 +52,7 @@ class SolrIndex(object):
                 r = self.client.request(method,fullpath,
                                         params=params,
                                         headers=headers,data=body)
-                
+        
                 if r.status_code == requests.codes.ok:
                     response = r.json()
                 else:
@@ -63,13 +67,28 @@ class SolrIndex(object):
 
     def _update(self,body):
         path = '%s/update/json' % self.collection
-        return self._send(path,method='POST',params={},body=body)
+        resp = self._send(path,method='POST',params={},body=body)
+        if type(resp) != type({}):
+            raise Exception(resp)
+        return resp
 
     def search(self,q,params={}):
         path = "%s/select" % self.collection
         params['q'] = q
-        data = SolrResponse(self._send(path,params))
-        return data
+        data = self._send(path,params)
+        if type(data) != type({}):
+            raise Exception(data)
+        
+        return SolrResponse(data)
+
+    def mlt(self,q,params={}):
+        path = "%s/mlt" % self.collection
+        params['q'] = q
+        data = self._send(path,params)
+        if type(data) != type({}):
+            raise Exception(data)
+        
+        return SolrResponse(data)
 
     def add(self,docs):
         message = json.dumps(docs,default=dthandler)
@@ -103,3 +122,99 @@ class SolrIndex(object):
     def commit(self):
         response = self._update('{"commit":{}}')
         return response
+
+
+@contextmanager
+def solr_batch_adder(solr, batch_size=2000, auto_commit=False):
+    batcher = SolrBatchAdder(solr, batch_size, auto_commit)
+    try:
+        yield batcher
+    finally:
+        log.info("solr_batch_adder: flushing last few items in batch")
+        batcher.flush()
+        if not auto_commit:
+            log.info("solr_batch_adder: auto_commit was False, issuing final commit")
+            batcher.commit()
+            
+class SolrBatchAdder(object):
+    def __init__(self, solr, batch_size=100, auto_commit=True):
+        """Provides an abstraction for batching commits to the Solr
+    index when processing
+        documents with pysolr.  `SolrBatchAdder` maintains an internal
+    "batch" list, and
+        when it reaches `batch_size`, it will commit the batch to
+    Solr.  This allows for
+        overall better performance when committing large numbers of
+    documents.
+
+        `batch_size` is 100 by default; different values may yield
+        different performance 
+        characteristics, and this of course depends upon your average
+        document size and 
+        Solr schema.  But 100 seems to improve performance
+        significantly over single commits."""
+        self.solr = solr
+        self.batch = list()
+        self.batch_len = 0
+        self.batch_size = batch_size
+        self.auto_commit = auto_commit
+
+    def add_one(self, doc):
+        """Adds a single document to the batch adder, committing only
+        if we've reached batch_size."""
+        self._append_commit(doc)
+
+    def add_multi(self, docs_iter):
+        """Iterates through `docs_iter`, appending each document to
+        the batch adder, committing mid-way
+        if batch_size is reached."""
+        assert hasattr(docs_iter, "__iter__"), "docs_iter must be iterable"
+        for doc in docs_iter:
+            self._append_commit(doc)
+
+    def flush(self):
+        """Flushes the batch queue of the batch adder; necessary after 
+        successive calls to `add_one` or `add_multi`."""
+        batch_len = len(self.batch)
+        auto_commit = self.auto_commit
+        log.debug("SolrBatchAdder: flushing {batch_len} articles to Solr (auto_commit={auto_commit})".format(
+            batch_len=batch_len, auto_commit=auto_commit))
+        try:
+            self.solr.add(self.batch)
+        except:
+            log.exception("Exception encountered when committing batch, falling back on one-by-one commit")
+            print "Exception encountered when committing batch, falling back on one-by-one commit"
+            # one by one fall-back
+            for item in self.batch:
+                try:
+                    self.solr.add([item])
+                except:
+                    log.error(u"Could not add item to solr index")
+                    log.exception(u"Exception stack trace for adding item")
+            if auto_commit:
+                self.commit()
+
+        self.batch = list()
+        self.batch_len = 0
+
+    def commit(self):
+        try:
+            self.solr.commit()
+        except :
+            log.warning("SolrBatchAdder timed out when committing, but   it's safe to ignore")
+
+    def _append_commit(self, doc):
+        if self.batch_len == self.batch_size:
+            # flush first, because we are at our batch size
+            self.flush()
+        self._add_to_batch(doc)
+
+    def _add_to_batch(self, doc):
+        self.batch.append(doc)
+        self.batch_len += 1
+
+    def __unicode__(self):
+        fmt = "SolrBatchAdder(batch_size={batch_size},  batch_len={batch_len}, solr={solr}"
+        return fmt.format(**vars(self))
+    
+   
