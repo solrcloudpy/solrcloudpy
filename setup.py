a53fcd5d71ae2fc746a635a6760d88e6d3059759
dfdeshom <didier@cogtree.com>
2013-11-27 12:06:34 -0600

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r') as f:
     
 setup(
     name = "solrcloudpy",
-    version = "1.0",
+    version = "1.0.1",
     author = 'Didier Deshommes',
     author_email = 'dfdeshom@gmail.com',
     packages=find_packages(exclude=['ez_setup']),
