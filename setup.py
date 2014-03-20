7b1f3e875ffc4bc075cf6fbd34f09dc087f2e711
dfdeshom <didier@cogtree.com>
2014-03-20 10:58:59 -0500

    upgrade requests lib

--- setup.py
+++ setup.py
@@ -29,6 +29,6 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Indexing/Search'
         ],
 
-    install_requires = ['requests == 1.2.0',],
+    install_requires = ['requests == 2.3.0',],
     extras_require = {"ip": ['IPython == 1.2.0'] }, 
         )
