4552ab00f83c8daccb7347818f3eb87866d0f292
dfdeshom <didier@cogtree.com>
2014-05-21 10:00:02 -0500

    increment version

--- solrcloudpy/__init__.py
+++ solrcloudpy/__init__.py
@@ -2,5 +2,5 @@ from solrcloudpy.connection import SolrConnection
 from solrcloudpy.collection import SolrCollection
 from solrcloudpy.parameters import SearchOptions
 
-__version__ = "1.3.460"
+__version__ = "1.4.460"
 __all__ = ['SolrCollection','SolrConnection','SearchOptions']
