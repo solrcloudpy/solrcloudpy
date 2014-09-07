7bf78387d54f9869bdc86cbca059afd23a54223b
Dexter Legaspi <dalegaspi@gmail.com>
2014-09-06 20:37:25 -0400

    Make the webappdir configurable
    make the webappdir part of the solr url requests configurable

--- solrcloudpy/connection.py
+++ solrcloudpy/connection.py
@@ -19,6 +19,7 @@ import json
 import solrcloudpy.collection as collection
 from solrcloudpy.utils import _Request
 
+
 class SolrConnection(object):
     """
     Connection to a solr server or several ones
@@ -29,18 +30,22 @@ class SolrConnection(object):
     :param user: HTTP basic auth user name
     :param password: HTTP basic auth password
     :param timeout: timeout for HTTP requests
+    :param webappdir: the solr webapp directory; defaults to 'solr'
     """
     def __init__(self,server="localhost:8983",
                  detect_live_nodes=False,
                  user=None,
                  password=None,
-                 timeout=10):
+                 timeout=10,
+                 webappdir='solr'):
         self.user = user
         self.password = password
         self.timeout = timeout
+        self.webappdir = webappdir
+        self.url_template = 'http://%%s/%s/' % self.webappdir
         
         if type(server) == type(''):
-            self.url = "http://%s/solr/" % server
+            self.url = self.url_template % server
             servers = [self.url,self.url]
             if detect_live_nodes:
                 url = servers[0]
@@ -48,7 +53,7 @@ class SolrConnection(object):
             else:
                 self.servers = servers
         if type(server) == type([]):
-            servers = ["http://%s/solr/" % a for a in server]
+            servers = [self.url_template % a for a in server]
             if detect_live_nodes:
                 url = servers[0]
                 self.servers = self.detect_nodes(url)
@@ -63,21 +68,21 @@ class SolrConnection(object):
         data = json.loads(live_nodes)
         children = [d['data']['title'] for d in data['tree'][0]['children']]
         nodes = [c.replace('_solr','') for c in children]
-        return ["http://%s/solr/" % a for a in nodes]
+        return [self.url_template % a for a in nodes]
 
     def list(self):
         """
         Lists out the current collections in the cluster
         """
         params = {'detail':'false','path':'/collections'}
-        response = self.client.get('/solr/zookeeper',params).result
+        response = self.client.get(('/%s/zookeeper' % self.webappdir),params).result
         data = response['tree'][0]['children']
         colls = [node['data']['title'] for node in data]
         return colls
 
     def _list_cores(self):
         params = {'wt':'json',}
-        response = self.client.get('admin/cores',params).result
+        response = self.client.get(('/%s/admin/cores' % self.webappdir),params).result
         cores = response.get('status',{}).keys()
         return cores
 
@@ -88,7 +93,7 @@ class SolrConnection(object):
         collections are returned, along with their state, otherwise an `OK` message is returned
         """
         params = {'detail':'true','path':'/clusterstate.json'}
-        response = self.client.get('/solr/zookeeper',params).result
+        response = self.client.get(('/%s/zookeeper' % self.webappdir),params).result
         data = json.loads(response['znode']['data'])
         res = []
         collections = self.list()
@@ -117,7 +122,7 @@ class SolrConnection(object):
         Gets the cluster leader
         """
         params = {'detail':'true','path':'/overseer_elect/leader'}
-        response = self.client.get('/solr/zookeeper',params).result
+        response = self.client.get(('/%s/zookeeper' % self.webappdir),params).result
         return json.loads(response['znode']['data'])
 
     @property
@@ -126,10 +131,10 @@ class SolrConnection(object):
         Lists all nodes that are currently online
         """
         params = {'detail':'true','path':'/live_nodes'}
-        response = self.client.get('/solr/zookeeper',params).result
+        response = self.client.get(('/%s/zookeeper' % self.webappdir),params).result
         children = [d['data']['title'] for d in response['tree'][0]['children']]
         nodes = [c.replace('_solr','') for c in children]
-        return ["http://%s/solr/" % a for a in nodes]
+        return [self.url_template % a for a in nodes]
 
     def create_collection(self,collname, *args, **kwargs):
         r"""
