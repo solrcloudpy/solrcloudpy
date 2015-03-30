2a4639da01b216ec36ccae6d3895c418a5cd4aac
dfdeshom <didier@cogtree.com>
2015-03-30 10:55:23 -0500

    update package

--- solrcloudpy/__init__.py
+++ solrcloudpy/__init__.py
@@ -2,5 +2,5 @@ from solrcloudpy.connection import SolrConnection
 from solrcloudpy.collection import SolrCollection
 from solrcloudpy.parameters import SearchOptions
 
-__version__ = "1.4.460"
-__all__ = ['SolrCollection','SolrConnection','SearchOptions']
+__version__ = "1.5.460"
+__all__ = ['SolrCollection', 'SolrConnection', 'SearchOptions']
